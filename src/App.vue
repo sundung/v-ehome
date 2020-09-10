8db0593ade3adbeae3fd3a22470f79ab2ede6d33
孙志强 <1181288394@qq.com>
2020-09-10 17:43:15 +0800

    按需引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    你好
     <router-view />
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { Button } from 'vant'
+
 Vue.config.productionTip = false
 
+Vue.use(Button)
 new Vue({
   router,
   store,
