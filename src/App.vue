e575199bfe62c0af83635b5c01bc85d766399556
孙志强 <1181288394@qq.com>
2020-10-16 16:42:33 +0800

    更改防盗链的轮播图连接

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,11 @@
 </template>
 
 <style lang="less">
+// 全局设置背景色
 body {
   background-color: #f8f8f8;
 }
+// 去掉 button的边框
 .van-button {
   border: none;
 }
--- src/components/Swipers.vue
+++ src/components/Swipers.vue
@@ -16,9 +16,9 @@ export default {
       goods: {
         // 轮播图图片
         thumbs: [
-          'https://yanxuan.nosdn.127.net/45b56c310ba2658f013a7847129cb4e2.jpg?type=webp&imageView&quality=75&thumbnail=750x0',
-          'https://yanxuan.nosdn.127.net/b6eb9066fb29c0fd14373e554b4cc838.jpg?type=webp&imageView&quality=75&thumbnail=750x0',
-          'https://yanxuan.nosdn.127.net/72440d65cbb26a337dbf84233d97e23b.jpg?type=webp&imageView&quality=75&thumbnail=750x0'
+          'https://img.yzcdn.cn/vant/apple-1.jpg',
+          'https://img.yzcdn.cn/vant/apple-2.jpg',
+          'https://img.yzcdn.cn/vant/apple-3.jpg'
         ]
       }
     }
